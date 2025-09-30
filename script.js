a2e61eefa604ea708ed29475d4703f7ffb9c081d
shinkensen <noobywinner587@gmail.com>
2025-09-29 20:42:09 -0500

    forgot to do commit almost. worked on js and adding functionality

--- script.js
+++ script.js
@@ -3,14 +3,17 @@ let gameArea = document.getElementById("game-area");
 let height=0;
 let start=false;
 let asteroidsX=[],asteroidsY=[],asteroidsSize=[]
+let health=3;
 document.addEventListener("keydown",function(event){
     if ((event.key=== 's')&&height<540){
         height+=10;
-        rocket.style.marginTop = `${height}px`;
+        rocket.style.transform = `rotate(45deg) translateY(${height}px)`;
+
     }
     if (event.key=== 'w' &&height>0){
         height-=10;
-        rocket.style.marginTop = `${height}px`;
+        rocket.style.transform = `rotate(45deg) translateY(${height}px)`;
+
     }
 })
 
@@ -18,13 +21,15 @@ function spawnAsteroid() {
     let asteroid = document.createElement("div");
     asteroid.classList.add("asteroid");
 
-    let y = Math.floor(Math.random() * 500);
+    let y = Math.floor(Math.random() * 750), w= Math.floor(Math.random() * 200)+50;;
     asteroid.style.top = `${y}px`;
-
+    asteroid.style.width = `${w}px`;
+    asteroid.style.height = `${w}px`;
     asteroid.style.left = "100%";
 
     gameArea.appendChild(asteroid);
     let x = window.innerWidth;
+    let done=false;
     let interval = setInterval(() => {
         x -= 5;
         asteroid.style.left = `${x}px`;
@@ -32,6 +37,12 @@ function spawnAsteroid() {
         clearInterval(interval);
         asteroid.remove();
         }
+        if (x<150 && (y+100>height&&y-100<height) &&!done){
+            asteroid.remove();
+            health--;
+            console.log(health);
+            done=true;
+        }
     }, 30);
 }
-setInterval(spawnAsteroid, 2000);
+setInterval(spawnAsteroid, 1000);
