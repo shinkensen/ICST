6cdebd70d46cd9b7cc1ad3118e1e098392c17129
shinkensen <noobywinner587@gmail.com>
2025-09-29 20:03:16 -0500

    got the up and down and the asteroids working

--- script.js
+++ script.js
@@ -1,4 +1,5 @@
 let rocket= document.getElementById("rocket");
+let gameArea = document.getElementById("game-area");
 let height=0;
 let start=false;
 let asteroidsX=[],asteroidsY=[],asteroidsSize=[]
@@ -12,3 +13,25 @@ document.addEventListener("keydown",function(event){
         rocket.style.marginTop = `${height}px`;
     }
 })
+
+function spawnAsteroid() {
+    let asteroid = document.createElement("div");
+    asteroid.classList.add("asteroid");
+
+    let y = Math.floor(Math.random() * 500);
+    asteroid.style.top = `${y}px`;
+
+    asteroid.style.left = "100%";
+
+    gameArea.appendChild(asteroid);
+    let x = window.innerWidth;
+    let interval = setInterval(() => {
+        x -= 5;
+        asteroid.style.left = `${x}px`;
+        if (x < -50) {
+        clearInterval(interval);
+        asteroid.remove();
+        }
+    }, 30);
+}
+setInterval(spawnAsteroid, 2000);
